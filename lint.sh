a51a70c8003972a21356a418d56c48d238702059
Michael Macnair <michael@macnair.org>
2017-02-18 19:50:32 +0000

    Lint all files separately, don't transpile.

--- lint.sh
+++ lint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Lint them separately, as closure doesn't understand node modules so detects false collisions
-for sources in "*.js" "test/*.js"
+# Lint all sources separately, as closure doesn't understand node modules so detects false collisions
+# This makes it sloooow
+# TODO with the feb version of closure, can use --module_resolution=node
+for sources in *.js test/*.js
 do
-  java -jar ./node_modules/google-closure-compiler/compiler.jar --jscomp_warning=lintChecks --checks_only $sources
+  java -jar ./node_modules/google-closure-compiler/compiler.jar --jscomp_warning=lintChecks --checks_only --language_out=ES5 $sources
 done
