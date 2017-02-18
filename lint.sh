99c25a0ed37cc7a3a83b9113ae30532f7c8c3987
Michael Macnair <michael@macnair.org>
2017-02-18 22:43:38 +0000

    Show progress when linting

--- lint.sh
+++ lint.sh
@@ -2,7 +2,8 @@
 # Lint all sources separately, as closure doesn't understand node modules so detects false collisions
 # This makes it sloooow
 # TODO with the feb version of closure, can use --module_resolution=node
-for sources in *.js test/*.js
+for f in *.js test/*.js
 do
-  java -jar ./node_modules/google-closure-compiler/compiler.jar --jscomp_warning=lintChecks --checks_only --language_out=ES5 $sources
+  echo $f
+  java -jar ./node_modules/google-closure-compiler/compiler.jar --jscomp_warning=lintChecks --checks_only --language_out=ES5 $f
 done
