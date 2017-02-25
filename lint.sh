718b687a8f6d0b847b2340c68d266087517537d6
Michael Macnair <michael@macnair.org>
2017-02-25 19:07:48 +0000

    Allow linting of single file

--- lint.sh
+++ lint.sh
@@ -2,8 +2,17 @@
 # Lint all sources separately, as closure doesn't understand node modules so detects false collisions
 # This makes it sloooow
 # TODO with the feb version of closure, can use --module_resolution=node
-for f in src/*.js test/*.js
-do
-  echo $f
-  java -jar ./node_modules/google-closure-compiler/compiler.jar --jscomp_warning=lintChecks --checks_only --language_out=ES5 $f
-done
+
+LINT_CMD="java -jar ./node_modules/google-closure-compiler/compiler.jar --jscomp_warning=lintChecks --checks_only --language_out=ES5"
+
+if [ $# -eq 1 ]
+then
+  echo $1;
+  $LINT_CMD $1;
+else
+  for f in src/*.js test/*.js
+  do
+    echo $f;
+    $LINT_CMD $f;
+  done
+fi
