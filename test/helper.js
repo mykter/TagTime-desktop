a07c61d8dc27c41ee08a7cfb747f14ea73c5fcd0
Michael Macnair <michael@macnair.org>
2017-03-10 12:19:45 +0000

    Revert appveyor npm change & use electron.cmd

--- test/helper.js
+++ test/helper.js
@@ -7,8 +7,8 @@ exports.appPath = path.resolve(__dirname, '../');
 
 var exe_suffix = "";
 if(process.platform === 'win32') {
-  exe_suffix = '.exe';
+  exe_suffix = '.cmd';
 }
-exports.electronPath = path.resolve(__dirname, '../node_modules/.bin/electron' + exe_suffix);
+exports.electronPath = path.resolve(__dirname, '..', 'node_modules', '.bin' , 'electron' + exe_suffix);
 
 process.env.NODE_ENV = 'test'; // suppress logging
