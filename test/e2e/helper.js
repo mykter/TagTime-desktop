a9df7635d3ad06eea849ed454fb0d9e682e82e76
Michael Macnair <michael@macnair.org>
2017-03-25 01:07:56 +0000

    Fix windows-only test code

--- test/e2e/helper.js
+++ test/e2e/helper.js
@@ -1,9 +1,10 @@
 const path = require('path');
 exports.appPath = path.resolve(__dirname, '..', '..');
 
-var exe_suffix = "";
+var electronPath;
 if(process.platform === 'win32') {
-  exe_suffix = '.cmd';
+  electronPath = path.resolve(__dirname, '..', '..', 'node_modules', 'electron', 'dist', 'electron.exe');
+} else {
+  electronPath = path.resolve(__dirname, '..', '..', 'node_modules', '.bin', 'electron');
 }
-exports.electronPath = path.resolve(__dirname, '..', '..', 'node_modules', 'electron', 'dist',
-                                    'electron.exe');
+exports.electronPath = electronPath;
