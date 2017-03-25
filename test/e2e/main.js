4f8d6f39c882ca74ee79fb8bcb1b85c9fc895500
Michael Macnair <michael@macnair.org>
2017-03-25 17:00:48 +0000

    Enable instance test on travis

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -56,70 +56,67 @@ describe('Application', function() {
   var app1, app2; // child_process
   var app1pid, app2pid;
 
-  if (process.env.DEBIAN_FRONTEND === 'noninteractive') {
-    // TODO this test times out on travis and appveyor
-    it('should only allow one instance to run');
-  } else {
-    it('should only allow one instance to run', function() {
-      this.timeout(10000);
+  it('should only allow one instance to run', function() {
+    this.timeout(10000);
 
-      app1 = child_process.spawn(electronPath, [ appPath, "--verbose" ]);
-      app1pid = app1.pid;
+    app1 = child_process.spawn(electronPath, [ appPath, "--verbose" ]);
+    app1pid = app1.pid;
 
-      return new Promise(function(fulfill, reject) {
-        var app1startup = function(buffer) {
-          if (buffer.toString().includes("Creating tray")) {
-            app2 = child_process.spawn(electronPath, [ appPath, '--verbose' ]);
-            app2pid = app2.pid;
-            app2.on('exit', function(code) {
-              fulfill(true);
-            });
+    return new Promise(function(fulfill, reject) {
+      var app1startup = function(buffer) {
+        if (buffer.toString().includes("Creating tray")) {
+          app2 = child_process.spawn(electronPath, [ appPath, '--verbose' ]);
+          app2pid = app2.pid;
+          app2.on('exit', function(code) { fulfill(true); });
 
-            // don't care which stream the notification will come on
-            app2.stdout.on('data', app2startup);
-            app2.stderr.on('data', app2startup);
-          }
-        };
+          // don't care which stream the notification will come on
+          app2.stdout.on('data', app2startup);
+          app2.stderr.on('data', app2startup);
+        }
+      };
 
-        var app2startup = function(buffer) {
-          if (buffer.toString().includes("starting up")) {
-            reject("Second instance is starting up");
-          }
-        };
+      var app2startup = function(buffer) {
+        if (buffer.toString().includes("starting up")) {
+          reject("Second instance is starting up");
+        }
+      };
 
-        // don't care which stream the notification will come on
-        app1.stdout.on('data', app1startup);
-        app1.stderr.on('data', app1startup);
-      });
+      // don't care which stream the notification will come on
+      app1.stdout.on('data', app1startup);
+      app1.stderr.on('data', app1startup);
     });
-  }
+  });
 
   afterEach(function() {
-      // Kill any processes spawned, and all their descendents.
-      // app[12].kill doesn't work - it kills the node process, but its
-      // descendants live on.
-      if (app1pid) { tree_kill(app1pid); }
-      if (app2pid) { tree_kill(app2pid); }
+    // Kill any processes spawned, and all their descendents.
+    // app[12].kill doesn't work - it kills the node process, but its
+    // descendants live on.
+    if (app1pid) {
+      tree_kill(app1pid);
+    }
+    if (app2pid) {
+      tree_kill(app2pid);
+    }
 
-      // Only move on from this test when all the processes spawned are dead.
-      // No longer sure if this is necessary, but keeping in case it is helping
-      // with hard-to-debug errors in CI.
-      return new Promise(function(resolve, reject) {
-        // resolve() if/when app2pid doesn't exist
-        var waitapp2 = function() {
-          if (app2pid) {
-            setTimeout(callWhenDead(app2pid, resolve), 100);
-          } else {
-            resolve();
-          }
-        };
-
-        // Once app1pid is gone, wait for app2pid
-        if (app1pid) {
-          setTimeout(callWhenDead(app1pid, waitapp2), 100);
+    // Only move on from this test when all the processes spawned are dead.
+    // No longer sure if this is necessary, but keeping in case it is helping
+    // with hard-to-debug errors in CI.
+    return new Promise(function(resolve, reject) {
+      // resolve() if/when app2pid doesn't exist
+      var waitapp2 = function() {
+        if (app2pid) {
+          setTimeout(callWhenDead(app2pid, resolve), 100);
         } else {
-          waitapp2();
+          resolve();
         }
-      });
+      };
+
+      // Once app1pid is gone, wait for app2pid
+      if (app1pid) {
+        setTimeout(callWhenDead(app1pid, waitapp2), 100);
+      } else {
+        waitapp2();
+      }
+    });
   });
 });
