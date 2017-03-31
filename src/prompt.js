c50ee66f475c33cfc16352ba73cf636b3c4cbdbf
Michael Macnair <michael@macnair.org>
2017-03-31 20:53:11 +0100

    Replace static libs dir with npm deps

--- src/prompt.js
+++ src/prompt.js
@@ -6,29 +6,31 @@ document.getElementById("time").innerHTML = moment(time).format('hh:mm:ss');
 
 /* Typeahead */
 
-const $ = window.$;
 const Bloodhound = window.Bloodhound;
-
 var allTags = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('tag'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  // Bloodhound doesn't work with a plain array of strings - needs key:value pairs
+  datumTokenizer : Bloodhound.tokenizers.obj.whitespace('tag'),
+  queryTokenizer : Bloodhound.tokenizers.whitespace,
+  // Bloodhound doesn't work with a plain array of strings - needs key:value
+  // pairs
   // https://github.com/twitter/typeahead.js/blob/master/doc/migration/0.10.0.md
-  local: [ "firsttag", "t:atool", "t:btool", "tags", "tastytags" ].map(
-     (e) => { return {tag : e}; }
-  ),
+  local : [ "firsttag", "t:atool", "t:btool", "tags", "tastytags" ].map(
+      (e) => { return {tag : e}; }),
 });
 
-$('input:first').tagsinput({
-  confirmKeys: [13, 32, 44], // space, comma, and enter trigger tag entry
-  trimValue: true,
-  cancelConfirmKeysOnEmpty: true,
-  typeaheadjs: {
-    name: 'allTags',
-    displayKey: 'tag',
-    valueKey: 'tag',
-    source: allTags.ttAdapter()
-  }
-});
+// document.getElementById doesn't work here - presumably tagsinput is extending
+// jQuery so #tags redirects to the new input element?
+const $ = window.$;
+$("#tags")
+    .tagsinput({
+      confirmKeys : [ 13, 32, 44 ], // space, comma, and enter trigger tag entry
+      trimValue : true,
+      cancelConfirmKeysOnEmpty : true,
+      typeaheadjs : {
+        name : 'allTags',
+        displayKey : 'tag',
+        valueKey : 'tag',
+        source : allTags.ttAdapter()
+      }
+    });
 
 $(".twitter-typeahead").css('display', 'inline');
