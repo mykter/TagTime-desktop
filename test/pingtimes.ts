0cc6ab5b02ba85354c588635bf8441ef4a69ed73
Max Beutelspacher <max.beutelspacher@ipa.fraunhofer.de>
2018-11-24 18:08:02 +0100

    add reproducibility tests for different seed

--- test/pingtimes.ts
+++ test/pingtimes.ts
@@ -136,14 +136,26 @@ describe("PingtimesReproducesOriginalImplementation", function() {
   });
 
   describe("next()", function() {
-    it("first ping should reproduce original implementation", function() {
+    it("first ping should be reproduced", function() {
       const time = PingTimes.epoch+1;
       pings.next(time).should.be.equal(1184098754*1000);
+    });
+
+    it("first ping should reproduced with different seed", function() {
+      const time = PingTimes.epoch+1;
+      pings.seed = 123456;
+      pings.next(time).should.be.equal(1184097486*1000);
     });
 
     it("ping should match original implementation in 2018",function(){
       const time = 1543080000*1000;
       pings.next(time).should.be.equal(1543081241*1000);
-    })
+    });
+
+    it("ping should match original implementation in 2018 with different seed",function(){
+      const time = 1543080000*1000;
+      pings.seed = 123456;
+      pings.next(time).should.be.equal(1543080933*1000);
+    });
   });
 });
